ca919bb450e703e76ade2e871d717dabd868169a
mtan09 <155262249+mtan09@users.noreply.github.com>
2026-02-17 18:54:13 -0500

    Add extra practice to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Michael Tan" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello, what's up!")
 
 if __name__ == "__main__":
     main()
